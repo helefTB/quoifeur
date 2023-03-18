7c46c378e8c347c1335d9c39e5d7a9ebcbbec1dc
helefTB <128255197+helefTB@users.noreply.github.com>
2023-03-18 20:51:00 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!'; // Le préfixe que vous avez choisi pour les commandes
+
+client.on('message', message => {
+  if (message.author.bot) return; // Le bot ne répondra pas à ses propres messages
+  if (!message.content.startsWith(prefix)) { // Vérifie si le message commence par le préfixe des commandes
+    const words = message.content.split(' ');
+    const lastWord = words[words.length - 1].toLowerCase();
+    if (lastWord === 'quoi') { // Vérifie si le dernier mot est "quoi"
+      message.channel.send('feur');
+    }
+  }
+});
+
+client.login('votre_token'); // Remplacez "votre_token" par le token de votre bot Discord
